4ad260b352eb3cebc88986f613b9e11c24cece7b
Jensen <suiterchik@gmail.com>
2019-09-25 22:20:12 +0800

    add header format

---
+++ src/libs/format/header.js
@@ -0,0 +1,50 @@
+let header = {
+    h1: {
+        block: 'h1',
+        styles: {
+            fontSize: '2.25em',
+            marginTop: '1rem',
+            marginBottom: '1rem',
+            fontWeight: 'bold',
+            borderBottom: '1px solid rgb(238, 238, 238)'
+        }
+    },
+    h2: {
+        block: 'h2',
+        styles: {
+            fontSize: '1.75em',
+            marginTop: '1rem',
+            marginBottom: '1rem',
+            fontWeight: 'bold',
+            borderBottom: '1px solid rgb(238, 238, 238)'
+        }
+    },
+    h3: {
+        block: 'h3',
+        styles: {
+            fontSize: '1.5em',
+            marginTop: '1rem',
+            marginBottom: '1rem',
+            fontWeight: 'bold'
+        }
+    },
+    h4: {
+        block: 'h4',
+        styles: {
+            fontSize: '1.25em',
+            marginTop: '1rem',
+            marginBottom: '1rem',
+            fontWeight: 'bold'
+        }
+    },
+    h5: {
+        block: 'h5',
+        styles: {
+            fontSize: '1em',
+            marginTop: '1rem',
+            marginBottom: '1rem',
+            fontWeight: 'bold'
+        }
+    },
+};
+export default header;
